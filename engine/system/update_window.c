2a91a11eb36222faa98a66f903e608f5f1289504
lagie-marin <marin.lagie@epitech.eu>
2024-04-13 23:18:37 +0200

    [UPDATE]add color for button

--- engine/system/update_window.c
+++ engine/system/update_window.c
@@ -10,13 +10,9 @@ static void render_element(canvas_t *canvas)
 {
     list_widgets_t *current;
 
-    my_printf("%i %i\n", canvas, canvas == NULL);
-    if (canvas == NULL) {
-        my_printf("canvas est terminer\n");
+    if (canvas == NULL)
         return;
-    }
     current = canvas->widgets;
-    printf("exist: %d\n", current->content);
     while (current != NULL && canvas->visible == TRUE) {
         if (current->type == CANVAS)
             render_element(current->content);
@@ -30,8 +26,6 @@ void update_window(void)
 {
     canvas_t **canvas = get_mapinfo()->canvas;
 
-    for (int i = 0; canvas[i]; i++) {
-        my_printf("---------%i---------\n", i);
+    for (int i = 0; canvas[i]; i++)
         render_element(canvas[i]);
-    }
 }
--- include/engine/color.h
+++ include/engine/color.h
@@ -7,8 +7,8 @@
 #include "core.h"
 #ifndef COLOR_H
     #define COLOR_H
-    #define GREY color(244, 244, 244, 1)
-    #define BEIGE color(234, 220, 166, 1)
-    #define GOLD color(226, 194, 117, 1)
-    #define ORANGE color(195, 106, 45, 1)
+    #define GREY color(244, 244, 244, 255)
+    #define BEIGE color(234, 220, 166, 255)
+    #define GOLD color(226, 194, 117, 255)
+    #define ORANGE color(195, 106, 45, 255)
 #endif
