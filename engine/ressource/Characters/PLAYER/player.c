feacaaa3943c5e9569ccb748e54722feebc3608a
Aldbg74 <alexis.drago-beltran@epitech.eu>
2024-05-06 16:32:49 +0200

    [ADD] Player

--- engine/ressource/Characters/PLAYER/player.c
+++ engine/ressource/Characters/PLAYER/player.c
@@ -5,7 +5,51 @@
 ** player.c
 */
 
-int player_function(void)
+#include <SFML/Graphics.h>
+#include <stdlib.h>
+
+
+typedef struct player_s {
+    sfSprite *sprite;
+    sfTexture *texture;
+    sfVector2f position;
+    int health;
+    int speed;
+} player_t;
+
+player_t* create_player(char *texture_path)
 {
-    return 0;
+    player_t *player = malloc(sizeof(player_t));
+    sfSprite_setTexture(player->sprite, player->texture, sfTrue);
+    player->texture = sfTexture_createFromFile(texture_path, NULL);
+    player->sprite = sfSprite_create();
+    player->position = (sfVector2f){0, 0};
+    player->health = 100;
+    player->speed = 10;
+    if (!player)
+        return NULL;
+    if (!player->texture) {
+        free(player);
+        return NULL;
+    }
+    return player;
+}
+
+void move_player(player_t *player, sfVector2f direction)
+{
+    player->position.x += direction.x * player->speed;
+    player->position.y += direction.y * player->speed;
+    sfSprite_setPosition(player->sprite, player->position);
+}
+
+void draw_player(sfRenderWindow* window, player_t *player)
+{
+    sfRenderWindow_drawSprite(window, player->sprite, NULL);
+}
+
+void destroy_player(player_t *player)
+{
+    sfTexture_destroy(player->texture);
+    sfSprite_destroy(player->sprite);
+    free(player);
 }
